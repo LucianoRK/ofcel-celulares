04ecd852b72734f18bfbd1ee5dca56bdba4b4597
LucianoPRK <lucianoprk@gmail.com>
2020-09-23 20:23:49 -0300

    Inicialização do projeto

---
+++ app/Controllers/UsuarioController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends BaseController
+{
+	public function __construct()
+	{
+		//
+	}
+	
+	/////////////////////////////
+	//                         //
+	//	        CRUD           //
+	//                         //
+	/////////////////////////////
+
+	/**
+	 * Pagina inicial do módulo
+	 */
+	public function index()
+	{
+		
+	}
+
+	/**
+	 * Mostra um item específico
+	 */
+	public function show()
+	{
+		//
+	}
+
+	/**
+	 * Retorna a View para criar um item da tabela
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Salva o novo item na tabela
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Retorna a View para edição do dado
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Salva a atualização do dado
+	 */
+	public function update()
+	{
+		//
+	}
+
+	/**
+	 * Remove ou desabilita o dado
+	 */
+	public function destroy()
+	{
+		//
+	}
+
+	/////////////////////////////
+	//                         //
+	//	   Outras funções      //
+	//                         //
+	/////////////////////////////
+}
---
+++ app/Views/home/index.php
@@ -0,0 +1,3 @@
+<section>
+    meio
+</section>
