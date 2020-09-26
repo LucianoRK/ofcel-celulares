26e930561693e2026c5848dbe9306be485242c1a
LucianoPRK <lucianoprk@gmail.com>
2020-09-26 13:25:45 -0300

    correção public

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -3,18 +3,18 @@
 </div>
 </div>
 </body>
-<script src="<?= base_url('public/js/core/jquery.3.2.1.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')?>"></script>
-<script src="<?= base_url('public/js/core/popper.min.js')?>"></script>
-<script src="<?= base_url('public/js/core/bootstrap.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/chartist/chartist.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/bootstrap-notify/bootstrap-notify.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/jquery-mapael/jquery.mapael.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/jquery-mapael/maps/world_countries.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/chart-circle/circles.min.js')?>"></script>
-<script src="<?= base_url('public/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')?>"></script>
-<script src="<?= base_url('public/js/ready.min.js')?>"></script>
+<script src="<?= base_url('js/core/jquery.3.2.1.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js')?>"></script>
+<script src="<?= base_url('js/core/popper.min.js')?>"></script>
+<script src="<?= base_url('js/core/bootstrap.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/chartist/chartist.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/chartist/plugin/chartist-plugin-tooltip.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/bootstrap-notify/bootstrap-notify.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/bootstrap-toggle/bootstrap-toggle.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/jquery-mapael/jquery.mapael.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/jquery-mapael/maps/world_countries.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/chart-circle/circles.min.js')?>"></script>
+<script src="<?= base_url('js/plugin/jquery-scrollbar/jquery.scrollbar.min.js')?>"></script>
+<script src="<?= base_url('js/ready.min.js')?>"></script>
 
 </html>
